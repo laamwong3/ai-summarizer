7e2a60d40ea9140d5d1b386f992903954ede3b0a
Laam Wong <laamwong3@gmail.com>
2023-04-24 07:59:46 +0800

    project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createTheme, NextUIProvider, useSSR } from "@nextui-org/react";
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import { trpc } from "@/utils/trpc";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+  type: "light",
+
+  theme: {
+    colors: {
+      background: "#26292B",
+    },
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  const { isBrowser } = useSSR();
+
+  return isBrowser ? (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  ) : null;
 }
+
+export default trpc.withTRPC(App);
